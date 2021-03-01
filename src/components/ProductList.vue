f32326027788ba1d5f353291764a8444d3f813f9
Michael Owuor <michael.owuor@gmail.com>
2021-03-01 20:59:31 +0300

    Dynamic Vuex getters.

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -5,7 +5,7 @@
 		<ul v-else>
 			<li v-for="product in products" :key="product.id">
 				{{ product.title }} - {{ product.price | currency }} - {{ product.inventory }}
-				<button @click="addProductToCart(product)">Add to Cart</button>
+				<button :disabled="!productIsInStock(product)" @click="addProductToCart(product)">Add to Cart</button>
 			</li>
 		</ul>
 	</div>
@@ -27,7 +27,11 @@ export default {
 
 	computed: {
 		products() {
-			return this.$store.getters.availableProducts;
+			return this.$store.state.products;
+		},
+
+		productIsInStock() {
+			return this.$store.getters.productIsInStock;
 		},
 	},
 
--- src/store/index.js
+++ src/store/index.js
@@ -33,6 +33,12 @@ export default new Vuex.Store({
 		cartTotal(state, getters) {
 			return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0);
 		},
+
+		productIsInStock() {
+			return (product) => {
+				return product.inventory > 0;
+			};
+		},
 	},
 
 	actions: {
@@ -46,18 +52,18 @@ export default new Vuex.Store({
 			});
 		},
 
-		addProductToCart(context, product) {
-			if (product.inventory > 0) {
+		addProductToCart({ state, getters, commit }, product) {
+			if (getters.productIsInStock(product)) {
 				// find cart item
-				const cartItem = context.state.cart.find((item) => item.id === product.id);
+				const cartItem = state.cart.find((item) => item.id === product.id);
 
 				if (!cartItem) {
-					context.commit('pushProductToCart', product.id);
+					commit('pushProductToCart', product.id);
 				} else {
-					context.commit('incrementItemQuantity', cartItem);
+					commit('incrementItemQuantity', cartItem);
 				}
 
-				context.commit('decrementProductInventory', product);
+				commit('decrementProductInventory', product);
 			}
 		},
 
